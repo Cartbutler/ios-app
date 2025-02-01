08d3ea97020ee5332d64907a4de49d4a628530c3
Cassiano Monteiro <cassiano.monteiro@faire.com>
2025-02-01 15:53:29 -0500

    Fix tab bar color

--- CartButler/CartButlerApp.swift
+++ CartButler/CartButlerApp.swift
@@ -10,15 +10,6 @@ import SwiftUI
 
 @main
 struct CartButlerApp: App {
-  
-  init() {
-    let tabBarAppearance = UITabBarAppearance()
-    tabBarAppearance.configureWithOpaqueBackground()
-    tabBarAppearance.backgroundColor = UIColor.themeBackground
-    UITabBar.appearance().standardAppearance = tabBarAppearance
-    UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
-  }
-  
   var body: some Scene {
     WindowGroup {
       MainView()
