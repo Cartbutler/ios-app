1e57fc84c59cd6fdcb05cfd9def8069ea32fc89b
Cassiano Monteiro <cassiano.monteiro@faire.com>
2025-02-07 21:26:08 -0500

    Created API service with fetchCategories method

---
+++ CartButler/Network/ApiService.swift
@@ -0,0 +1,24 @@
+//
+//  ApiService.swift
+//  CartButler
+//
+//  Created by Cassiano Monteiro on 2025-02-05.
+//
+
+import Foundation
+
+protocol APIServiceProvider: Sendable {
+  func fetchCategories() async throws -> [CategoryDTO]
+}
+
+final class APIService: APIServiceProvider {
+  private let apiClient: APIClientProvider
+
+  init(apiClient: APIClientProvider = APIClient.shared) {
+    self.apiClient = apiClient
+  }
+
+  func fetchCategories() async throws -> [CategoryDTO] {
+    try await apiClient.get(path: "categories")
+  }
+}
